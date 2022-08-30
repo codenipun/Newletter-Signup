834c7bbd5e7c60b0e5b893846aeab674a1058609
Nipun Jain <nipunjain119@gmail.com>
2022-08-30 19:38:12 +0530

    fourth comit

--- app.js
+++ app.js
@@ -41,15 +41,14 @@ app.post("/", function(req, res){
         auth : "nipun1:c093c0c9d9afb2d7c415daca5c49f712-us8"
     }
     const request = https.request(url, options, function(response){
-        if(response.statusCode===200){
-            res.sendFile(__dirname + "/success.html")
-        }else{
-            res.sendFile(__dirname + "/failure.html");
-        }
-        
-        response.on("data", function(data){
-            console.log(JSON.parse(data));
-        })
+         response.on("data", function(data){
+            sub_data = JSON.parse(data)
+            if(response.statusCode===200){
+                res.sendFile(__dirname + "/success.html")
+            }else{
+                res.sendFile(__dirname + "/failure.html");
+            }
+        });
     });
 
     request.write(jsonData);
